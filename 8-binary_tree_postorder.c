7cd79ceabde1be3b385a05b649f4c1110a6770c7
Bane <121007715+Banedb@users.noreply.github.com>
2025-07-03 18:57:55 +0100

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -3,6 +3,7 @@
 /**
  * binary_tree_postorder - Traverses a binary tree post-orderly.
  * @tree: Root node of the tree to traverse.
+ * @func: Pointer to a function to call for each node.
  */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
