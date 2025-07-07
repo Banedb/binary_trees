877a6c2d6b119c7ddc815c9100f5fe0e6a5a2fec
Bane <121007715+Banedb@users.noreply.github.com>
2025-07-07 16:49:24 +0100

    Add files via upload

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -34,8 +34,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree->left && !tree->right)
 		return (1);
-	if (tree->left && tree->right &&
-	    height(tree->left) == height(tree->right))
+
+	if (height(tree->left) != height(tree->right))
+		return (0);
+	if (tree->left && tree->right)
 		return (binary_tree_is_perfect(tree->left) &&
 			binary_tree_is_perfect(tree->right));
 	return (0);
