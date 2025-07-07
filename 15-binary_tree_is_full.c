24ca1278d44ba70c469f57592b4542c26c620760
Bane <121007715+Banedb@users.noreply.github.com>
2025-07-07 13:13:23 +0100

    15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - Checks if a binary tree is full.
+ * @tree: Root node of the tree to traverse.
+ *
+ * Return: balance of the tree || 0 if tree is NULL.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (!tree)
+		return (0);
+
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+	if ((!left && right) || (left && !right))
+		return (0);
+	if ((tree->left && !tree->right) || (!tree->left && tree->right))
+		return (0);
+       	return (1);
+}
